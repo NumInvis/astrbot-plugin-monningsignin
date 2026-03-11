f53fbef34681aa29e874e48e1b14fa5e66ebff70
User <user@example.com>
2026-03-12 03:00:35 +0800

    feat: 添加插件市场支持文件

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+莫宁宁的币 - AstrBot 经济系统插件
+版本: 1.0
+作者: NumInvis
+"""
+
+__version__ = "1.0"
+__author__ = "NumInvis"
+__plugin_name__ = "莫宁宁的币"
+__description__ = "一个功能丰富的 AstrBot 经济系统插件，包含签到、银行、商店、股票、工作、结社、成就、好感度等多个子系统"
